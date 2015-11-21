995a1c56feeb5423f7ff6bb96d0ce237a27d91e0
RedouaneMA <r.makhtouri@gmail.com>
2015-11-21 12:42:38 +0100

    Revert "Version 0"
    This reverts commit 93bcbd31e231c6ff2b83cc879d483e7f4894791a.

--- Frontend/.tmp/templates.js
+++
@@ -1,8 +0,0 @@
-angular.module('app').run(['$templateCache', function($templateCache) {
-  'use strict';
-
-  $templateCache.put('main/todo.html',
-    "je suis todo"
-  );
-
-}]);
--- Frontend/app/generated-config.js
+++
@@ -1,5 +0,0 @@
-angular.module('app.config', [])
-
-.constant('serverBaseUrl', 'http://localhost:8080/bfm/rest')
-
-;
